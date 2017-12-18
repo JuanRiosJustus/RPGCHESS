0e90951f281e79c65bef764f46009e7a0809ee65
Justus Brown <JustusBrownCP@Outlook.com>
2017-12-18 00:41:16 -0500

    type initializer fix

---
+++ RPGChess/Graphics/ImageManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+public class ImageManager
+{
+    private static ImagePool IM;
+
+    private static ImagePool GetImagePoolInstance()
+    {
+        if (IM == null)
+        {
+            IM = new ImagePool();
+        }
+        return IM;
+    }
+    /// <summary>
+    /// returns the main background for the program.
+    /// </summary>
+    /// <returns></returns>
+    public static Bitmap MainBackground()
+    {
+        GetImagePoolInstance();
+        return IM.GetMainBackground();
+    }
+    /// <summary>
+    /// Returns teh secondary image for secondary forms for the program.
+    /// </summary>
+    /// <returns></returns>
+    public static Bitmap SubBackground()
+    {
+        GetImagePoolInstance();
+        return IM.GetSubBackground();
+    }
+    /// <summary>
+    /// Returns a bitmap based on the given archetype and relatio.
+    /// </summary>
+    /// <param name="class_type">archetype to find</param>
+    /// <param name="relation">relation to find.</param>
+    /// <returns></returns>
+    public static Bitmap DetermineCharacterImage(Archetype class_type, Relation relation)
+    {
+        GetImagePoolInstance();
+        return IM.GetImage(class_type.NAME, relation);
+    }
+    /// <summary>
+    /// Returns the appropriate image of the givne biome.
+    /// </summary>
+    /// <param name="biome"> the String representation of the biome. </param>
+    /// <returns></returns>
+    public static Bitmap DetermineBiomeImage(String biome)
+    {
+        GetImagePoolInstance();
+        return IM.GetImage(biome);
+    }
+    /// <summary>
+    /// Resize the image to the specified width and height.
+    /// </summary>
+    /// <param name="image">The image to resize.</param>
+    /// <param name="width">The width to resize to.</param>
+    /// <param name="height">The height to resize to.</param>
+    /// <returns>The resized image.</returns>
+    public static Bitmap ResizeImage(Image image, int width, int height)
+    {
+        var destRect = new Rectangle(0, 0, width, height - 5);
+        var destImage = new Bitmap(width, height);
+
+        destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+        using (var graphics = Graphics.FromImage(destImage))
+        {
+            graphics.ScaleTransform(1.50f, 1.665f);
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            using (var wrapMode = new ImageAttributes())
+            {
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height + 5, GraphicsUnit.Pixel, wrapMode);
+            }
+        }
+
+        return destImage;
+    }
+}
---
+++ RPGChess/Graphics/ImagePool.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ImagePool
+{
+    public class ImagePoolNode
+    {
+        public readonly Bitmap Picture;
+        public readonly string Name;
+        public readonly Relation Relation;
+
+        public ImagePoolNode(Bitmap picture, string name, Relation relation)
+        {
+            Picture = picture;
+            Name = name; // <?>
+            Relation = relation;
+        }
+    }
+
+    public ImagePool()
+    {
+        MainBackground = new Bitmap(@"..\..\Assets\Backgrounds\MainBG.PNG");
+        SubBackground = new Bitmap(@"..\..\Assets\Backgrounds\SubBG.PNG");
+
+        Bitmaps = new List<ImagePoolNode>();
+        
+        /********** GET ALL IMAGES **********/
+        /// TILE IMAGES
+        DirectoryInfo dir = new DirectoryInfo(@"..\..\Assets\Tiles\");
+        FileInfo[] Files = dir.GetFiles(@"*.PNG");
+        foreach (FileInfo file in Files)
+        {
+            string path = @"..\..\Assets\Tiles\" + file.Name;
+            string name = file.Name.Substring(0, file.Name.IndexOf("."));
+            Bitmap b = new Bitmap(path);
+            ImagePoolNode ipn = new ImagePoolNode(b, name, Relation.Enemy);
+            Bitmaps.Add(ipn);
+        }
+
+        /// FRIENDLY IMAGES
+
+        dir = new DirectoryInfo(@"..\..\Assets\Characters\FRN\");
+        FileInfo[] Files1 = dir.GetFiles(@"*.PNG");
+        foreach (FileInfo file in Files1)
+        {
+            string path = @"..\..\Assets\Characters\FRN\" + file.Name;
+            string name = file.Name.Substring(0, file.Name.IndexOf("."));
+            Console.WriteLine(file.Name);
+            Bitmap b = new Bitmap(path);
+            ImagePoolNode ipn = new ImagePoolNode(b,name, Relation.Friendly);
+            Bitmaps.Add(ipn);
+        }
+
+        /// ENEMY IMAGES
+        
+        dir = new DirectoryInfo(@"..\..\Assets\Characters\NME\");
+        FileInfo[] Files2 = dir.GetFiles(@"*.PNG");
+        foreach (FileInfo file in Files2)
+        {
+            string path = @"..\..\Assets\Characters\NME\" + file.Name;
+            string name = file.Name.Substring(0, file.Name.IndexOf("."));
+            Console.WriteLine(file.Name);
+            Bitmap b = new Bitmap(path);
+            ImagePoolNode ipn = new ImagePoolNode(b, name, Relation.Enemy);
+            Bitmaps.Add(ipn);
+        }
+
+    }
+
+    private Bitmap MainBackground;
+    private Bitmap SubBackground;
+
+    private List<ImagePoolNode> Bitmaps;
+
+    public Bitmap GetMainBackground()
+    {
+        return MainBackground;
+    }
+    public Bitmap GetSubBackground()
+    {
+        return SubBackground;
+    }
+    public Bitmap GetImage(int index)
+    {
+        return Bitmaps[index].Picture;
+    }
+    public Bitmap GetImage(string name, Relation relation)
+    {
+        for (int i = 0; i < Bitmaps.Count; i++)
+        {
+            if (Bitmaps[i].Name.ToLower().Contains(name.ToLower()))
+            {
+                if (Bitmaps[i].Relation == relation)
+                {
+                    return Bitmaps[i].Picture;
+                }
+            }
+        }
+        return null;
+    }
+    public Bitmap GetImage(string name)
+    {
+        for (int i = 0; i < Bitmaps.Count; i++)
+        {
+            if (Bitmaps[i].Name.ToLower().Contains(name.ToLower()))
+            {
+                return Bitmaps[i].Picture;
+            }
+        }
+        return null;
+    }
+}
